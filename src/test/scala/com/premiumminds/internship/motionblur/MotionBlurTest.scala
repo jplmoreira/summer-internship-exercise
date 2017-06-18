2268cd314a6b2de0617cd640ab21275c74ed2b9a
jplmoreira <jplmoreira92@gmail.com>
2017-06-18 15:54:36 +0100

    Add additional tests

--- src/test/scala/com/premiumminds/internship/motionblur/MotionBlurTest.scala
+++ src/test/scala/com/premiumminds/internship/motionblur/MotionBlurTest.scala
@@ -19,26 +19,61 @@ class MotionBlurTest extends FunSuite {
     *
     */
   val implementations: Map[String, MotionBlurFactory] =
-    Map(
-      "single thread" -> MotionBlurSingleThread,
-      "multi thread" -> MotionBlurMultiThread)
+    Map("single thread" -> MotionBlurSingleThread,
+        "multi thread" -> MotionBlurMultiThread)
 
   implementations.foreach { case (name, implementation) =>
 
     test(s"[$name] Motion blur with M1=\n${prettyPrint(MatrixData.M1)}\n") {
       val step1 = implementation.run(MatrixData.M1, 5)
-      val result = Await.result(step1, 10.seconds)
+      val result1 = Await.result(step1, 10.seconds)
 
-      assert(result === List(
+      assert(result1 === List(
         List(3, 3, 4),
         List(4, 5, 6),
         List(6, 7, 8)
       ))
     }
 
-    /**
-      * Implement more tests...
-      */
+    test(s"[$name] Motion blur with M2=\n${prettyPrint(MatrixData.M2)}\n") {
+      val step2 = implementation.run(MatrixData.M2, 5)
+      val result2 = Await.result(step2, 10.seconds)
+      
+      assert(result2 === List(
+        List(11, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 12, 13, 13, 13),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
+        List(14, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10),
+        List(11, 14, 13, 13, 13, 12, 12, 12, 11, 11, 11, 10, 10, 10, 9, 9, 9, 8, 8, 8)
+      ))
+    }
+
+    test(s"[$name] Motion blur with M3=\n${prettyPrint(MatrixData.M3)}\n") {
+      val step3 = implementation.run(MatrixData.M3, 5)
+      val result3 = Await.result(step3, 10.seconds)
+
+      assert(result3 === List(
+        List(2, 2, 3, 4, 5),
+        List(2, 3, 4, 5, 6),
+        List(3, 4, 5, 6, 7),
+        List(4, 4, 5, 6, 7)
+      ))
+    }
   }
 
   def prettyPrint(data: Seq[Seq[Int]]) = data.map(_.mkString(" ")).mkString("\n")
