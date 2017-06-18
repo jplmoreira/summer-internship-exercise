b2211fa60701d45d20d74b05b520d1d4978b6495
jplmoreira <jplmoreira92@gmail.com>
2017-06-18 15:54:01 +0100

    Implement single and multithread motionblur calculation

--- src/main/scala/com/premiumminds/internship/motionblur/MotionBlurSingleThread.scala
+++ src/main/scala/com/premiumminds/internship/motionblur/MotionBlurSingleThread.scala
@@ -1,5 +1,8 @@
 package com.premiumminds.internship.motionblur
 
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+
 /**
   * Created by acamilo on 18-04-2016.
   */
@@ -10,5 +13,14 @@ object MotionBlurSingleThread extends MotionBlurFactory {
     * @param numberOfWorkers this parameter should be ignored
     * @return matrix of integers
     */
-  def run(data: Seq[Seq[Int]], numberOfWorkers: Int) = ???
+  def run(data: Seq[Seq[Int]], numberOfWorkers: Int) = {
+    implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
+    Future {
+      data.zipWithIndex.map {
+        case (line, x) => line.zipWithIndex.map {
+          case (_, y) => calculateMotionBlur(data, x, y)
+        }
+      }
+    }
+  }
 }
